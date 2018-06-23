509cc5395f5d1afa1f0def6d4e10ada9d3444281
Alice RUSSO <arusso@e2r12p12.42.fr>
2018-06-23 13:17:14 +0200

    first

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/28 15:46:58 by arusso            #+#    #+#             */
+/*   Updated: 2018/06/23 12:52:10 by arusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+
+# define BUFF_SIZE 4
+
+int		get_next_line(const int fd, char **line);
+
+#endif
